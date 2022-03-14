c79ea9af60c74c67d37d9b78727f7b289842f72c
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-14 16:47:27 +0100

    Extend color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'purple': '#ce357f',
+      'midnight': '#15032e',
+      'bermuda': '#752fee',
+      'sun' : '#fbe752',
+      'orange': '#f4b95b',
+      'red': '#ec5cae',
+      'pink':'#e838df',
+    },},
   },
   plugins: [],
 };
