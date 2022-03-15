ca1b5b18f7e8074cd8efeba2dbe53b8865005f0e
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-15 15:19:00 +0100

    Add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'purple': '#ce357f',
-      'midnight': '#15032e',
-      'bermuda': '#752fee',
-      'sun' : '#fbe752',
-      'orange': '#f4b95b',
-      'red': '#ec5cae',
-      'pink':'#e838df',
-    },},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        purple: "#ce357f",
+        midnight: "#15032e",
+        bermuda: "#752fee",
+        sun: "#fbe752",
+        orange: "#f4b95b",
+        nova: "#f09652",
+        red: "#eb038d",
+        pink: "#e838df",
+        teal: "#7df9ff",
+      },
+      boxShadow: {
+        glow: "0 0 .2rem #000, 0 0 .2rem #000, 0 0 2rem #752fee, 0 0 0.8rem #752fee, 0 0 1.5rem #752fee, inset 0 0 1.3rem #752fee",
+        glowOrange: "0 0 .2rem #000, 0 0 .2rem #000, 0 0 2rem #f4b95b, 0 0 0.8rem #f4b95b, 0 0 1.5rem #f4b95b, inset 0 0 1.3rem #f4b95b",
+      },
+      fontFamily: {
+        'eightyone': ['Indelible'],
+        'eightynine': ['Commando']
+      }
+    },
   },
   plugins: [],
 };
