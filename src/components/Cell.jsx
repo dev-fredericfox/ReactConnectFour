a83666bf6b73e40c12c4b23fcee3d11f4ce4b32e
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-17 12:08:48 +0100

    Change extension and destructure props

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,15 +1,15 @@
 import Coin from "./Coin";
 
-function Cell(props) {
+function Cell({setCoin, column, turn, cellStatus}) {
   return (
     <div
       onClick={() => {
-        props.setCoin(props.column, props.turn);
+        setCoin(column,turn);
       }}
       className="flex flex-row bg-midnight w-11 h-11 sm:w-14 sm:h-14 m-[1.5px]"
     >
-      {!props.cellStatus && <Coin />}
-      <Coin color={props.cellStatus} size="big" />
+      {cellStatus && <Coin />}
+      <Coin color={cellStatus} size="big" />
     </div>
   );
 }
